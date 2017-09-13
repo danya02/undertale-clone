9b8620347023adabc640908bc5e98ba15d508563
Danya Generalov <danya02@me.com>
2017-09-13 21:43:33 +0300

    Adding optional text to dog error handler.

--- main.py
+++ main.py
@@ -10,16 +10,20 @@ scale_factor = 4
 s1 = pygame.transform.scale(s1, (s1.get_width()*scale_factor, s1.get_height()*scale_factor))
 s2 = pygame.transform.scale(s2, (s2.get_width()*scale_factor, s2.get_height()*scale_factor))
 
-def invoke_dog():
+def invoke_dog(text=""):
     pygame.mixer.music.load("mus/mus_dance_of_dog.ogg")
+    font = pygame.font.Font("fonts/determinationmono.ttf", 32)
+    text = font.render(text, 0, pygame.Color(255,255,255,255))
     pygame.mixer.music.play(-1)
     while 1:
         d.fill(pygame.Color(0,0,0,255))
         d.blit(s1, (400 - int(s1.get_width()/2), 300 - int(s1.get_height()/2)))
+        d.blit(text, (400 - int(text.get_width()/2), 450))
         pygame.display.update()
         pygame.time.wait(250)
         d.fill(pygame.Color(0,0,0,255))
         d.blit(s2, (400 - int(s1.get_width()/2), 300 - int(s1.get_height()/2)))
+        d.blit(text, (400 - int(text.get_width()/2), 450))
         pygame.display.update()
         pygame.time.wait(250)
-invoke_dog()
+invoke_dog("Not implemented.")
