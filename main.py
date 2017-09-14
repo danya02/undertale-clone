af7fb7bd5ad020e58a9180f7ff5fa2e1856861c8
Danya Generalov <danya02@me.com>
2017-09-14 22:22:05 +0300

    Internalizing functions to load dog.

--- main.py
+++ main.py
@@ -3,14 +3,12 @@ import pygame
 pygame.init()
 d = pygame.display.set_mode((800,600))
 
-s1 = pygame.image.load("sprites/spr_tobdogl_0.png")
-s2 = pygame.image.load("sprites/spr_tobdogl_1.png")
-
-scale_factor = 4
-s1 = pygame.transform.scale(s1, (s1.get_width()*scale_factor, s1.get_height()*scale_factor))
-s2 = pygame.transform.scale(s2, (s2.get_width()*scale_factor, s2.get_height()*scale_factor))
-
 def invoke_dog(text=""):
+    s1 = pygame.image.load("sprites/spr_tobdogl_0.png")
+    s2 = pygame.image.load("sprites/spr_tobdogl_1.png")
+    scale_factor = 4
+    s1 = pygame.transform.scale(s1, (s1.get_width()*scale_factor, s1.get_height()*scale_factor))
+    s2 = pygame.transform.scale(s2, (s2.get_width()*scale_factor, s2.get_height()*scale_factor))
     pygame.mixer.music.load("mus/mus_dance_of_dog.ogg")
     font = pygame.font.Font("fonts/determinationmono.ttf", 32)
     text = font.render(text, 0, pygame.Color(255,255,255,255))
