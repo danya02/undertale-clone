2f419d93d42cf1246fe2945f1cb84c6371b299f0
Danya Generalov <danya02@me.com>
2017-10-20 21:44:19 +0300

    Adding an SFX getter module.

---
+++ sfx.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+import pygame
+import os
+sounds = {}
+
+for i in os.listdir('./sfx/'):
+    sounds.update({int(i.split('.')[0], 16): pygame.Sound('./sfx/'+i)}
+
+def get_sound(sound):
+    """
+    Return a Sound with this identifier.
+    Sound can be specified by int or a '0x' identifier.
+    """
+    return sounds[int(sound, 0)]
