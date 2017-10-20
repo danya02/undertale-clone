331c66af646bd81660214922172828b02f84877b
Danya Generalov <danya02@me.com>
2017-10-20 21:34:54 +0300

    Adding docstrings, improving robustness and general refactors.

--- main.py
+++ main.py
@@ -3,110 +3,157 @@ from pygame.locals import *
 import sys
 import gzip
 import threading
-import frisk
+
+# our modules are imported below the invoke_dog function
 
 pygame.init()
-d = pygame.display.set_mode((800,600))
+d = pygame.display.set_mode((800, 600))
+
+global chara
+chara = None
+global running
+running = True
+
 
 def scale(img, times):
-    return pygame.transform.scale(img, (int(img.get_width()*times), int(img.get_height()*times)))
+    return pygame.transform.scale(img, (int(img.get_width() * times), int(img.get_height() * times)))
+
 
 def invoke_dog(text="", type=0):
-    s1 = pygame.image.fromstring(gzip.decompress(b'\x1f\x8b\x08\x00\xd8\xb8\xbbY\x02\xffc`\xa0\x14\xfc\xff\xff\x1fB\xc2\x01\\\x90\x18\xbdX\x01%z\tjGv$V\xc7\x93g).\x13\xe0"h\n\x881\x01Y1ZP\x13\xe3\x184\xff\x92\xe1\x8b\xffd\x81\x11\xa5\x97\xd4\x80\xc5\xaf\x17\x7f\x94\xe1\x91\xc5\xaa\x12\xab^\x82y\x19\xbf\x08\xe5\x00\x00W\x07lAt\x04\x00\x00'), (20, 19), 'RGB')
-    s2 = pygame.image.fromstring(gzip.decompress(b'\x1f\x8b\x08\x00\x85\xb9\xbbY\x02\xffc`\xa0\x0e\xf8\xff\xff?\x84\x84\x03\xb8 \xf1&`\x05\x94\x9b@\xa4!\xc8\xce\xc6\xea\x1dJ\x1c@\xa49h\xca\xc80\x079\xe4\xb1z\x84\xa0\x99\x98\xe6\x93\xea\xaf\xffd\x81Q\x13\xb0F+\xb5L\xc0\x1f\xb9\xb8d\xb1\xaa\xc1\x9a\xe4\x08\x9a@RIB]\x00\x00h\x1d1\xcc\xe6\x04\x00\x00'), (22, 19), 'RGB')
+    """
+    Show the Annoying Dog with optional text until user quits.
+    Use this as a fatal error handler, or if the SAVE file is FUBAR.
+    Supposed to work under as few assumptions as possible.
+    """
+    s1 = pygame.image.fromstring(gzip.decompress(
+        b'\x1f\x8b\x08\x00\xd8\xb8\xbbY\x02\xffc`\xa0\x14\xfc\xff\xff\x1fB\xc2\x01\\\x90\x18\xbdX\x01%z\tjGv$V\xc7'
+        b'\x93g).\x13\xe0"h\n\x881\x01Y1ZP\x13\xe3\x184\xff\x92\xe1\x8b\xffd\x81\x11\xa5\x97\xd4\x80\xc5\xaf\x17\x7f'
+        b'\x94\xe1\x91\xc5\xaa\x12\xab^\x82y\x19\xbf\x08\xe5\x00\x00W\x07lAt\x04\x00\x00'),
+        (20, 19), 'RGB')
+    s2 = pygame.image.fromstring(gzip.decompress(
+        b'\x1f\x8b\x08\x00\x85\xb9\xbbY\x02\xffc`\xa0\x0e\xf8\xff\xff?\x84\x84\x03\xb8 '
+        b'\xf1&`\x05\x94\x9b@\xa4!\xc8\xce\xc6\xea\x1dJ\x1c@\xa49h\xca\xc80\x079\xe4\xb1z\x84\xa0\x99\x98\xe6\x93\xea'
+        b'\xaf\xffd\x81Q\x13\xb0F+\xb5L\xc0\x1f\xb9\xb8d\xb1\xaa\xc1\x9a\xe4\x08\x9a@RIB]\x00\x00h\x1d1\xcc\xe6\x04'
+        b'\x00\x00'),
+        (22, 19), 'RGB')
     s1 = scale(s1, 4)
     s2 = scale(s2, 4)
-    s3 = pygame.image.fromstring(gzip.decompress(b'\x1f\x8b\x08\x00\xe1!\xd9Y\x02\xffc`\x18\x05\x03\t\xfec\x00\xaa\x18\x82\x07\xd0\xc2L"\rGS\x89\xc6 \xdbd\xac\xc6\x12i2yQC\xd0\n"\xcd\x84\x88 \x93\xc8\xdai\x11\x08d$9\xac\xea\xf1\x84\t\x19\xe1\x80\'vp\x89\xd0:c\x02\x00\x85\xf4\xb8\xa3\x19\x05\x00\x00'), (29,15), "RGB")
-    s4 = pygame.image.fromstring(gzip.decompress(b"\x1f\x8b\x08\x00\xe7!\xd9Y\x02\xffc`\x18\x05C\x1e\xfc'\x1a\xd0\xc2L\xe2\xcdGS\x89\xc6 \xdbd\xac\xc6\x12i2I!@\xd0F\x82\xc6b*C#\xd1L\xa0E \xe02\x16\x97\xf9X}G0X\xf0\xbb\x99\xa4\x14E^\x02\xa6\x10\x00\x00K\\\xfb\xa9\x19\x05\x00\x00"), (29,15), "RGB")
+    s3 = pygame.image.fromstring(gzip.decompress(
+        b'\x1f\x8b\x08\x00\xe1!\xd9Y\x02\xffc`\x18\x05\x03\t\xfec\x00\xaa\x18\x82\x07\xd0\xc2L"\rGS\x89\xc6 '
+        b'\xdbd\xac\xc6\x12i2yQC\xd0\n"\xcd\x84\x88 '
+        b'\x93\xc8\xdai\x11\x08d$9\xac\xea\xf1\x84\t\x19\xe1\x80\'vp\x89\xd0:c\x02\x00\x85\xf4\xb8\xa3\x19\x05\x00'
+        b'\x00'),
+        (29, 15), "RGB")
+    s4 = pygame.image.fromstring(gzip.decompress(
+        b"\x1f\x8b\x08\x00\xe7!\xd9Y\x02\xffc`\x18\x05C\x1e\xfc'\x1a\xd0\xc2L\xe2\xcdGS\x89\xc6 "
+        b"\xdbd\xac\xc6\x12i2I!@\xd0F\x82\xc6b*C#\xd1L\xa0E "
+        b"\xe02\x16\x97\xf9X}G0X\xf0\xbb\x99\xa4\x14E^\x02\xa6\x10\x00\x00K\\\xfb\xa9\x19\x05\x00\x00"),
+        (29, 15), "RGB")
     s3 = scale(s3, 4)
     s4 = scale(s4, 4)
     try:
         pygame.mixer.music.load(["mus/mus_dance_of_dog.ogg", "mus/mus_sigh_of_dog.ogg"][type])
-    except:
+    except pygame.error:
         pass
-    font = pygame.font.Font("fonts/determinationmono.ttf", 32)
-    text = font.render(text, 0, pygame.Color(255,255,255,255))
+    try:
+        font = pygame.font.Font("fonts/determinationmono.ttf", 32)
+    except OSError:
+        font = pygame.font.SysFont(pygame.font.get_default_font(), 32)
+    text = font.render(text, 0, pygame.Color(255, 255, 255, 255))
     try:
         pygame.mixer.music.play(-1)
-    except:
+    except pygame.error:
         pass
     while 1:
         for event in pygame.event.get():
-            if event.type==QUIT:
+            if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == KEYDOWN:
-                if (event.key == K_ESCAPE):
+                if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
-        d.fill(pygame.Color(0,0,0,255))
-        d.blit([s1,s3][type], (400 - int([s1,s3][type].get_width()/2), 300 - int([s1,s3][type].get_height()/2)))
-        d.blit(text, (400 - int(text.get_width()/2), 450))
+        d.fill(pygame.Color(0, 0, 0, 255))
+        d.blit([s1, s3][type], (400 - int([s1, s3][type].get_width() / 2), 300 - int([s1, s3][type].get_height() / 2)))
+        d.blit(text, (400 - int(text.get_width() / 2), 450))
         pygame.display.update()
-        pygame.time.wait([250,500][type])
-        d.fill(pygame.Color(0,0,0,255))
-        d.blit([s2,s4][type], (400 - int([s1,s3][type].get_width()/2), 300 - int([s1,s3][type].get_height()/2)))
-        d.blit(text, (400 - int(text.get_width()/2), 450))
+        pygame.time.wait([250, 500][type])
+        d.fill(pygame.Color(0, 0, 0, 255))
+        d.blit([s2, s4][type], (400 - int([s1, s3][type].get_width() / 2), 300 - int([s1, s3][type].get_height() / 2)))
+        d.blit(text, (400 - int(text.get_width() / 2), 450))
         pygame.display.update()
-        pygame.time.wait([250,500][type])
+        pygame.time.wait([250, 500][type])
 
-def intro(type=0):
-    if type == 0: # normal intro
+
+try:
+    import frisk
+except ImportError as e:
+    invoke_dog("ImportError: "+str(e))
+
+
+def intro(version=0):
+    """
+    Play the Undertale intro, as seen on program start. Return when the intro is done.
+    """
+    if version == 0:  # normal intro
         pygame.mixer.music.load("mus/mus_story_91.ogg")
         images = []
         for i in range(11):
-            images += [pygame.image.load("sprites/spr_introimage_"+str(i)+".png")]
+            images += [pygame.image.load("sprites/spr_introimage_" + str(i) + ".png")]
         images = [scale(i, 3) for i in images]
         delays = [8000, 7000, 8000, 6000, 6000, 6000, 8000, 6000, 8000, 8000, 15000]
         pygame.mixer.music.play()
-        for i,j in zip(images, delays):
-            d.blit(i, (400-(i.get_width()/2), 0))
+        for i, j in zip(images, delays):
+            d.blit(i, (400 - (i.get_width() / 2), 0))
             pygame.display.update()
             pygame.time.delay(j)
         pygame.mixer.music.load("mus/mus_intronoise.ogg")
         i = pygame.image.load("sprites/splash.png")
         i = scale(i, 2.5)
-        d.blit(i, (400-(i.get_width()/2), 300-(i.get_height()/2)))
+        d.blit(i, (400 - (i.get_width() / 2), 300 - (i.get_height() / 2)))
         pygame.display.update()
         pygame.mixer.music.play()
         pygame.time.wait(5000)
-    elif type == 1: # hijacked by Flowey EX
+    elif version == 1:  # hijacked by Flowey EX
         pygame.mixer.music.load("mus/mus_story_91.ogg")
         images = []
         for i in ["_0", "2_0"]:
-            images += [scale(pygame.image.load("sprites/spr_fakeintro"+str(i)+".png"), 2.5)]
+            images += [scale(pygame.image.load("sprites/spr_fakeintro" + str(i) + ".png"), 2.5)]
         pygame.mixer.music.play()
-        d.blit(images[0], (400-(images[0].get_width()/2), 0))
+        d.blit(images[0], (400 - (images[0].get_width() / 2), 0))
         pygame.display.update()
         pygame.time.delay(6000)
         pygame.time.delay(2000)
         pygame.mixer.music.load("mus/mus_story_stuck.ogg")
         pygame.mixer.music.play(-1)
-        d.blit(images[1], (400-(images[1].get_width()/2), 0))
+        d.blit(images[1], (400 - (images[1].get_width() / 2), 0))
         pygame.display.update()
         pygame.time.wait(5000)
-    elif type == 2: # world destroyed
+    elif version == 2:  # world destroyed
         pygame.mixer.music.load("mus/mus_wind.ogg")
         pygame.mixer.music.play(-1)
         for i in range(600):
             pygame.time.wait(1000)
+
+
 def init():
     global chara
     chara = frisk.Frisk()
     global running
     running = True
 
+
 def spritecycle():
     global chara
     scale_factor = 2
-    upcycle = [scale(pygame.image.load("sprites/spr_maincharau_"+str(i)+".png"), scale_factor) for i in range(4)]
-    downcycle = [scale(pygame.image.load("sprites/spr_maincharad_"+str(i)+".png"), scale_factor) for i in range(4)]
-    leftcycle = [scale(pygame.image.load("sprites/spr_maincharal_"+str(i)+".png"), scale_factor) for i in range(2)]
-    rightcycle = [scale(pygame.image.load("sprites/spr_maincharar_"+str(i)+".png"), scale_factor) for i in range(2)]
+    upcycle = [scale(pygame.image.load("sprites/spr_maincharau_" + str(i) + ".png"), scale_factor) for i in range(4)]
+    down_cycle = [scale(pygame.image.load("sprites/spr_maincharad_" + str(i) + ".png"), scale_factor) for i in range(4)]
+    left_cycle = [scale(pygame.image.load("sprites/spr_maincharal_" + str(i) + ".png"), scale_factor) for i in range(2)]
+    right_cycle = [scale(pygame.image.load("sprites/spr_maincharar_" + str(i) + ".png"), scale_factor) for i in range(2)]
     while running:
-        chara.sprite = [upcycle,rightcycle,downcycle,leftcycle][chara.dir][0]
+        chara.sprite = [upcycle, right_cycle, down_cycle, left_cycle][chara.dir][0]
         if chara.moving:
-            for i in [upcycle,rightcycle,downcycle,leftcycle][chara.dir]:
+            for i in [upcycle, right_cycle, down_cycle, left_cycle][chara.dir]:
                 if not chara.moving:
                     break
                 chara.sprite = i
@@ -114,21 +161,22 @@ def spritecycle():
                     if not chara.moving:
                         break
                     pygame.time.wait(10)
+
 def maincycle():
     global chara
     sprite_cycle = threading.Thread(target=spritecycle)
     sprite_cycle.start()
     pygame.time.wait(250)
     while running:
-        d.fill(pygame.Color(0,0,0,0))
-        #d.blit(background,(0,0))
+        d.fill(pygame.Color(0, 0, 0, 0))
+        # d.blit(background,(0,0))
         d.blit(chara.sprite, (int(chara.pos[0]), int(chara.pos[1])))
         for event in pygame.event.get():
-            if event.type==QUIT:
+            if event.type == QUIT:
                 pygame.quit()
                 sys.exit()
             if event.type == KEYDOWN:
-                if (event.key == K_ESCAPE):
+                if event.key == K_ESCAPE:
                     pygame.quit()
                     sys.exit()
         keys_pressed = pygame.key.get_pressed()
@@ -136,33 +184,36 @@ def maincycle():
         if keys_pressed[K_LEFT]:
             chara.dir = 3
             chara.moving = True
-            chara.pos = (chara.pos[0]-0.5, chara.pos[1])
+            chara.pos = (chara.pos[0] - 0.5, chara.pos[1])
         if keys_pressed[K_RIGHT]:
             chara.dir = 1
             chara.moving = True
-            chara.pos = (chara.pos[0]+0.5, chara.pos[1])
+            chara.pos = (chara.pos[0] + 0.5, chara.pos[1])
         if keys_pressed[K_UP]:
             chara.dir = 0
             chara.moving = True
-            chara.pos = (chara.pos[0], chara.pos[1]-0.5)
+            chara.pos = (chara.pos[0], chara.pos[1] - 0.5)
         if keys_pressed[K_DOWN]:
             chara.dir = 2
             chara.moving = True
-            chara.pos = (chara.pos[0], chara.pos[1]+0.5)
+            chara.pos = (chara.pos[0], chara.pos[1] + 0.5)
         pygame.display.update()
         if not int(chara.pos[0]) in range(800) or not int(chara.pos[1]) in range(600):
             raise NotImplementedError("chara left screen area", 1)
-try:
-    init()
-    #intro(0)
-    maincycle()
-except:
-    e = sys.exc_info()
+
+
+if __name__ == "__main__":
     try:
-        invoke_dog(type(e[1]).__name__+": "+str(eval(str(e[1]))[0]), int(eval(str(e[1]))[1]))
-    except SystemExit:
-        sys.exit()
+        init()
+        # intro(0)
+        maincycle()
     except:
-        invoke_dog(type(e[1]).__name__+": "+str(e[1]))
-finally:
-    run = False
+        e = sys.exc_info()
+        try:
+            invoke_dog(type(e[1]).__name__ + ": " + str(eval(str(e[1]))[0]), int(eval(str(e[1]))[1]))
+        except SystemExit:
+            sys.exit()
+        except:
+            invoke_dog(type(e[1]).__name__ + ": " + str(e[1]))
+    finally:
+        run = False
